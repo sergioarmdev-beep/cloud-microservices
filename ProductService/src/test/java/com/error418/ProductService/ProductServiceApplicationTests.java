3a4c4510c39d3db3daf87aeb897b7d7ac268a849
sergioarmdev-beep <sergio.a.r.m.dev@gmail.com>
2026-09-24 12:06:55 -0400

    Initial commit: microservicio básico con Spring Boot

---
+++ ProductService/src/test/java/com/error418/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.error418.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ orderService/src/test/java/com/error418/orderService/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.error418.orderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
